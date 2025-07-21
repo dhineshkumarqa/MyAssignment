700adf4c1dcde230de82be54350a7758738360bd
dhineshkumarqa <dhinesh.kumar.qa14@gmail.com>
2025-07-21 22:57:47 +0530

    Update FindOddNumbers.java

--- day2/FindOddNumbers.java
+++ day2/FindOddNumbers.java
@@ -3,19 +3,20 @@ package week1.day2;
 public class FindOddNumbers {
 
 	public static void main(String[] args) {
-		/*
-		 * for (int i = 1; i <= 10; i++) { if (i % 2 != 0) { System.out.println(i); } }
-		 */
-		String name = "Dhinesh";
-		for(int i=name.length()-1;i>=0;i--)
-		{
-			char ch = name.charAt(i);
-            System.out.print(ch);
+		
+		  for (int i = 1; i <= 10; i++) { 
+			  if (i % 2 != 0) 
+			  { 
+				  System.out.println(i); 
+			  } 
+		  }
+		 
+
 		}
 		
 	}
 
-}
+
 
 
 
